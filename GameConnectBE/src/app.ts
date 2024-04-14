989e6ac4e386a5fd5337da6c0ef5b24366bcb657
Daniel Král <kraldaniel.cze@gmail.com>
2024-04-14 14:35:32 +0200

    Update app.ts

--- GameConnectBE/src/app.ts
+++ GameConnectBE/src/app.ts
@@ -5,6 +5,7 @@ import { authenticateToken } from './middlewares/authenticateToken.middleware'
 import { pageRouter } from './components/Page/page.routes'
 import { menuItemRouter } from './components/MenuItem/menuItem.routes'
 import { postRouter } from './components/Post/post.routes'
+import { commentRouter } from './components/Comment/comment.routes'
 
 export const app = express()
 app.use(express.json())
@@ -13,3 +14,4 @@ app.use('/user', authenticateToken, userRouter)
 app.use('/page', authenticateToken, pageRouter)
 app.use('/menu-item', authenticateToken, menuItemRouter)
 app.use('/post', authenticateToken, postRouter)
+app.use('/comment', authenticateToken, commentRouter)
